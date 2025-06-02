2016be38b1d8b6d5ea07245a96635d3117f9f5d5
Timothy Nakev <99489258+MyNamesTim@users.noreply.github.com>
2025-06-02 09:18:18 -0400

    Added vectors and comments, made changes to directional movement, fixes still needed and velocity/acceleration still needed

--- Camel.js
+++ Camel.js
@@ -1,3 +1,10 @@
+/**
+* Name: Timothy Nakev
+* Program Name: Creature Lab (Camel)
+* Date: 6/2/2025
+* Extra: On random occasions, camels will stop and starting eating crops off of the ground.
+*/
+
 class Camel {
   constructor() {
     //set this.position.y to equal the height of the ground
@@ -8,19 +15,22 @@ class Camel {
     this.easing = 0.05; 
     this.camelDirection = 1;
     this.display = true;
-    this.size = 3; 
+    this.size = createVector(3, 3); 
   }
 
   show() {
     push();
+    //use velocity and acceleration instead
     translate(this.position.x, this.position.y); 
-    scale(this.camelDirection * this.size, this.size); 
+    //scale() used because original sprite was too small. is there, perhaps, a way to turn off terture filtering so the sprite is crisp?
+    scale(this.camelDirection * this.size.x); 
     image(this.camel_img, 0, 0); 
     pop();
   }
 
   update() {
     let newTargetX = noise(this.xoff) * width;
+    //lerp() used for smooth movements from place to place instead of snappy motions
     this.targetX = lerp(this.targetX, newTargetX, 0.02);
 
     this.position.x = lerp(this.position.x, this.targetX, this.easing);
