88419be11a026fc665eb6440b65973f2432b5978
Timothy Nakev <99489258+MyNamesTim@users.noreply.github.com>
2025-06-02 12:43:39 -0400

    Make final changes, add extra

--- Camel.js
+++ Camel.js
@@ -7,40 +7,72 @@
 
 class Camel {
   constructor() {
-    //set this.position.y to equal the height of the ground
-    this.position = createVector(random(width), height/2)
+    this.position = createVector(random(width), height * 0.75);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
     this.xoff = random(1000);
     this.camel_img = loadImage("assets/camel.png");
-    this.targetX = this.position.x; 
-    this.easing = 0.05; 
-    this.camelDirection = 1;
-    this.display = true;
-    this.size = createVector(3, 3); 
+    this.camel_eating_img = loadImage("assets/camel_eating.png");
+    this.maxSpeed = 2;
+    this.accelerationForce = 0.05; // defines the strength of the camel's acceleration towards the target
+    this.size = 3;
+    this.eatingChance = 0;
+    this.eatingTimer = 0;
+    imageMode(CENTER);
   }
 
   show() {
     push();
-    //use velocity and acceleration instead
-    translate(this.position.x, this.position.y); 
-    //scale() used because original sprite was too small. is there, perhaps, a way to turn off terture filtering so the sprite is crisp?
-    scale(this.camelDirection * this.size.x); 
-    image(this.camel_img, 0, 0); 
+    translate(this.position.x, this.position.y);
+
+    if (this.velocity.x < 0) {
+      scale(-this.size, this.size);
+    } else {
+      scale(this.size, this.size);
+    }
+
+    this.eatingChance = random(0, 10);
+
+    if(this.eatingChance % 2 == 0) {
+      image(this.camel_eating_img, 0, 0);
+    } else {
+      image(this.camel_img, 0, 0);
+    }
     pop();
   }
 
   update() {
-    let newTargetX = noise(this.xoff) * width;
-    //lerp() used for smooth movements from place to place instead of snappy motions
-    this.targetX = lerp(this.targetX, newTargetX, 0.02);
+    let targetX = noise(this.xoff) * width;
 
-    this.position.x = lerp(this.position.x, this.targetX, this.easing);
+    let targetDirection = createVector(targetX - this.position.x, 0);
 
-    if (this.targetX > this.position.x + 1) { 
-      this.camelDirection = 1;
-    } else if (this.targetX < this.x - 1) {
-      this.camelDirection = -1;
+    if (targetDirection.x > 0) {
+      this.acceleration.x = this.accelerationForce;
+    } else if (targetDirection.x < 0) {
+      this.acceleration.x = -this.accelerationForce;
+    } else {
+      this.acceleration.x = 0;
     }
 
-    this.xoff += 0.005; 
+    this.velocity.add(this.acceleration);
+
+    if (abs(this.velocity.x) > this.maxSpeed) {
+      this.velocity.x = this.maxSpeed * (this.velocity.x > 0 ? 1 : -1);
+    }
+    this.velocity.y = 0;
+
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+    this.xoff += 0.005;
+
+    // checks if the camel has reached the boundary yet
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+
+    // keeps the y position consistent (camel stays on the ground)
+    this.position.y = height * 0.75;
   }
 }
