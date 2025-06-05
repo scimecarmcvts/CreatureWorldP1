d581ad83bac57cbcaee45f7021079a18665bebad
julian12o <julian.noahortega@icloud.com>
2025-06-05 09:09:59 -0400

    Update Alien.js

--- Alien.js
+++ Alien.js
@@ -1,22 +1,26 @@
 class Alien {
   constructor() {
     this.position = createVector(random(width), random(height));
-    this.velocity = createVector(2, -2);
+    this.velocity = createVector(random(2, -2), random(-2,1));
     this.acceleration = createVector(-0.01, 0.01);
-    this.img = loadImage("alien with ufo.png");
+    this.img = loadImage("assets/ufo.png");
   }
 
   update() {
     this.velocity.add(this.acceleration);
-    this.velocity.limit(10);
+    this.velocity.limit(2);
     this.position.add(this.velocity);
-  
+    if (this.position.x > width){
+      this.position.x = width
+      this.velocity.x *= -1
+    }
+   if (this.position.x < 0){
+    this.position.x = 0
+    this.velocity.x *= -1
   }
 
+  }
   show() {
-    stroke(255);
-    fill(0);
-    strokeWeight(2)
-    image(this.position.x, this.position.y, 2.5, 2.5);
+    image(this.img,this.position.x, this.position.y/20, 50, 50);
   }
 }
