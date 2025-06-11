5a7d616e2597b68508b4adb79273e4196da3f4b2
Flix <nsulagodu@gmail.com>
2025-06-11 09:03:13 -0400

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -1,27 +1,39 @@
 class Snake {
   constructor(x, y, img) {
-     this.gridSize = 100;
-     this.x = x;
-       this.y = y;
-     this.img = img;
-    this.noiseOffsetX = 0;
-       this.noiseOffsetY = 1000;
+    this.gridSize = 100;
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D().mult(2);
+    this.img = img;
+    this.noiseOffset = random(1000);
   }
+
+
   update() {
-    
-    this.noiseOffsetX += 0.01;
-       this.noiseOffsetY += 0.01;
-
-  
-    let angle = noise(this.noiseOffsetX) * TWO_PI;
-     this.x += cos(angle) * 2;
-       this.y += sin(angle) * .002;
-
-
-        this.x = constrain(this.x, 0, width - this.gridSize);
-    this.y = constrain(this.y, 0, height - this.gridSize);
+    this.noiseOffset += 0.01;
+    let angleWiggle = map(noise(this.noiseOffset), 0, 1, -0.05, 0.05);
+    this.vel.rotate(angleWiggle);
+    this.vel.setMag(2);
+    this.pos.add(this.vel);
+    if (this.pos.x < 0 || this.pos.x > width - this.gridSize) {
+      this.vel.x *= -1;
+      this.pos.x = constrain(this.pos.x, 0, width - this.gridSize);
+    }
+    if (this.pos.y < 0 || this.pos.y > height - this.gridSize) {
+      this.vel.y *= -1;
+      this.pos.y = constrain(this.pos.y, 0, height - this.gridSize);
+    }
+    if (this.pos.y < 310) {
+      this.pos.y = 310;
+    }
   }
+//I added an extra which is a color tint to objects based of the mouse
   show() {
-                image(this.img, this.x, this.y, this.gridSize, this.gridSize);
+    let d = dist(mouseX, mouseY, this.pos.x, this.pos.y);
+    if (d < 150) {
+      tint(255, 100, 100);
+    } else {
+      noTint();
+    }
+    image(this.img, this.pos.x, this.pos.y, this.gridSize, this.gridSize);
   }
 }
